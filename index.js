286d6e7e1d187e164e22c6257d4e1278efbb48d6
Andreas Mairhofer <andreas@classphp.de>
2015-02-01 18:15:29 +0100

    fix(): more fixes to include skeleton-stylus in stylus middleware

--- index.js
+++ index.js
@@ -1,15 +1,33 @@
 
 var path = require('path');
+var stylus = require('stylus');
+var utils = stylus.utils;
 
 /**
  * Library version.
  */
 
-exports.version = require(path.join(__dirname, '../package.json')).version;
+exports.version = require(path.join(__dirname, 'package.json')).version;
 
 /**
  * Stylus path.
  */
 
 exports.path = __dirname;
+
+exports = module.exports = plugin;
+
+
+/**
+ * Return the plugin callback for stylus.
+ *
+ * @return {Function}
+ * @api public
+ */
+
+function plugin() {
+  return function(style){
+    style.include(__dirname);
+  };
+}
 
