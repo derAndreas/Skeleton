0717719df1e9bf5577fdf358fbc23f01b95ef7ff
Andreas Mairhofer <andreas@classphp.de>
2015-02-24 07:37:27 +0100

    fix(build): include normalize, remove unnecessary build step

--- gulpfile.js
+++ gulpfile.js
@@ -4,17 +4,9 @@ var stylus = require('gulp-stylus');
 var rename = require('gulp-rename');
 var csso = require('gulp-csso');
 
-gulp.task('normalize.css', function() {
-  return gulp.src('node_modules/normalize.css/normalize.css')
-    .pipe(gulp.dest('dist/'))
-    .pipe(csso())
-    .pipe(rename({suffix: '.min'}))
-    .pipe(gulp.dest('./dist/'));
-});
-
-gulp.task('build', ['normalize.css'], function() {
+gulp.task('build', function() {
   return gulp.src('index.styl')
-    .pipe(stylus())
+    .pipe(stylus({'include css': true}))
     .pipe(rename('skeleton.css'))
     .pipe(gulp.dest('./dist/'))
     .pipe(csso())
